108584340e3a8005c841b9e2752154cb20606bc7
abye12345 <tensaeabye@gmail.com>
2024-12-22 09:35:44 +0300

    commit

--- AbsoluteSumMinimizatio.js
+++ AbsoluteSumMinimizatio.js
@@ -1,1 +1,14 @@
+"use strict";
 let Array = [1, 2, 3, 4, 5];
+let len = Array.length;
+let i;
+for (i = 0; i < len; i++) {
+  let a = Array[i];
+  let summin =
+    Math.abs(Array[0] - a) +
+    Math.abs(Array[1] - a) +
+    Math.abs(Array[2] - a) +
+    Math.abs(Array[3] - a) +
+    Math.abs(Array[4] - a);
+  console.log(summin);
+}
