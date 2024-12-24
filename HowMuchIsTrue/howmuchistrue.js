1650bba7bd9bfb935fadc450dbb52abef8b3dfb4
abye12345 <tensaeabye@gmail.com>
2024-12-24 21:15:07 +0300

    commit

--- HowMuchIsTrue/howmuchistrue.js
+++ HowMuchIsTrue/howmuchistrue.js
@@ -2,7 +2,7 @@ function Array() {
   let arr = [true, false, true, true, false];
   let count = 0;
   for (i = 0; i < arr.length; i++) {
-    if (arr[i] === false) {
+    if (arr[i] === true) {
       count++;
     }
   }
