e5e441372ae1394f024d67f12b77481f84bb8c64
abye12345 <tensaeabye@gmail.com>
2025-01-07 22:22:01 +0300

    commit

--- 100-JavaScript-Algorithm-challenge/instance/instance.js
+++ 100-JavaScript-Algorithm-challenge/instance/instance.js
@@ -1,1 +1,18 @@
-
+function instant(arr) {
+  let Arr = [];
+  let len = arr.length;
+  for (i = 0; i < len; i++) {
+    let x = arr[i];
+    let y = x.charAt(x.length - 1);
+    if (y != "7") {
+      let result = x.concat("7");
+      Arr.push(result);
+    } else {
+      result = x;
+      Arr.push(result);
+    }
+  }
+  return Arr;
+}
+let output = instant(["gssh", "xhdhd", "dudu", "gdhd7"]);
+console.log(output);
