d2be7e4e4fa19d9ff1a2e84a9376a193cff8e09d
abye12345 <tensaeabye@gmail.com>
2025-01-15 22:26:27 +0300

    commit

---
+++ 100-JavaScript-Algorithm-challenge/Something in the Box/README.md
@@ -0,0 +1,1 @@
+53. Create a function that returns true if an asterisk \* is inside a box
---
+++ 100-JavaScript-Algorithm-challenge/Something in the Box/Something in the Box.js
@@ -0,0 +1,5 @@
+function astriks(arr) {
+  return arr.includes("*");
+}
+let result = astriks([3, 9, 89, "hello", "*"]);
+console.log(result);
